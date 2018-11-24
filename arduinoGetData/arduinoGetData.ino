1c6d57ab67a24db616c7059bc424ca9771234e08
karolinadrobotowicz <karolina.drobotowicz@gmail.com>
2018-11-24 22:00:06 +0200

    full states definition

--- arduinoGetData/arduinoGetData.ino
+++ arduinoGetData/arduinoGetData.ino
@@ -1,6 +1,12 @@
-#define no_state 0
-#define ent100 1
-#define ent200 2
+#define s_nothing '0' // ent [0-10]
+#define s_heatingup '1' //ent [10-100]
+#define s_meh '2' // ent [100-150]
+#define s_warm '3' // ent [150-200]
+#define s_hot '4' // ent [200 - 250]
+#define s_sizzle '5' // ent [250 - 300]
+#define s_hell '6' // ent>300
+#define s_loyly '7' 
+#define s_openDoor '8'
 
 void setup() {
   // put your setup code here, to run once:
@@ -17,8 +23,35 @@ void loop() {
     str[1] = '\0';
     Serial.print(str);
 
-    if(data == ent200)
-    
+    switch(state){
+      case s_nothing:
+        //do something
+        break;
+      case s_heatingup:
+        //do something
+        break;
+      case s_meh:
+        //do something
+        break;
+      case s_warm:
+        //do something
+        break;
+      case s_hot:
+        //do something
+        break;
+      case s_sizzle:
+        //do something
+        break;
+      case s_hell:
+        //do something
+        break;
+      case s_loyly:
+        //do something
+        break;
+      case s_openDoor:
+        //do something
+        break;
+    }
   }
 }
 
