455fe18c492c93017ba30c1e3b7d0c7fc230e032
karolinadrobotowicz <karolina.drobotowicz@gmail.com>
2018-11-24 22:20:47 +0200

    program now checks for the current state

---
+++ consts.py
@@ -0,0 +1,24 @@
+states ={
+    's_nothing': '0',
+    's_heatingup': '1',
+    's_meh': '2',
+    's_warm': '3',
+    's_hot': '4',
+    's_sizzle': '5',
+    's_hell':'6',
+    's_loyly': '7',
+    's_openDoor': '8'
+}
+
+maxEnt ={
+    's_nothing': 10,
+    's_heatingup': 100,
+    's_meh': 150,
+    's_warm': 200,
+    's_hot': 250,
+    's_sizzle': 300
+}
+
+#s_hell = 700
+#define s_loyly '7'
+#define s_openDoor '8'
--- serialArduino.py
+++ serialArduino.py
@@ -3,6 +3,7 @@
 
 ## Serial to Arduino
 import getSensorData as gsd
+import consts
 import serial
 import time
 import struct
@@ -23,21 +24,63 @@ def get_enthalpy2():
     bench_data = gsd.get_sensor_data('Bench2', 1)
     return bench_data[0]['Measurements']['Temperature']['value']
 
+
+def loyly():
+    sensordata = gsd.get_sensor_data('Stove1', 3)
+    return gsd.check_for_loyly(sensordata[0], sensordata[-1])
+
+def doorOpen():
+    sensordata = gsd.get_sensor_data('Doorway1', 4)
+    return gsd.check_for_opendoor(sensordata[0], sensordata[-1])
+
+
+def check_state():
+
+    if(loyly()):
+        print("loyly")
+        return consts.states['s_loyly']
+    if(doorOpen()):
+        print("door open")
+        return consts.states['s_openDoor']
+
+    ent = get_enthalpy()
+    if ent < consts.maxEnt['s_nothing']:
+        return consts.states['s_nothing']
+
+    if ent < consts.maxEnt['s_heatingup']:
+        print('s_heatingup')
+        return consts.states['s_heatingup']
+    if ent < consts.maxEnt['s_meh']:
+        print('s_meh')
+        return consts.states['s_meh']
+    if ent < consts.maxEnt['s_warm']:
+        print('s_warm')
+        return consts.states['s_warm']
+    if ent < consts.maxEnt['s_hot']:
+        print('s_hot')
+        return consts.states['s_hot']
+    if ent < consts.maxEnt['s_sizzle']:
+        print('s_sizzle')
+        return consts.states['s_sizzle']
+    return consts.states['hell']
+
+
+
+
 while True:
 
-    ## Get the sensor data
-    ent = get_enthalpy()
-    ent2 = get_enthalpy2()
+    ## Check the state
+    state = check_state()
+
     if(not ard_connected):
-        print(ent)
-        print(ent2)
+        print(state)
         time.sleep(1)
 
     ## recognize if 0 - nothing, 1 - normal, 2 - loyly, 3 - open door
 
     if(ard_connected):
         ## Send the data
-        arduino.write(str.encode(str(ent)))
+        arduino.write(str.encode(str(state)))
         #arduino.write("Hello from Python!")
         print("sent")
         data = arduino.readline()
@@ -45,5 +88,5 @@ while True:
         if data:
             print("data")
             print(data.decode())  # strip out the new lines for now
-            time.sleep(2)
+            time.sleep(1)
             # (better to do .read() in the long run for this reason
