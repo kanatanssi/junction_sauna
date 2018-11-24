afbd2532949dcd8196ef39357abbcee6e7c1bac8
karolinadrobotowicz <karolina.drobotowicz@gmail.com>
2018-11-24 20:11:51 +0200

    added getting the data

--- serialArduino.py
+++ serialArduino.py
@@ -2,28 +2,41 @@
 # give information outside
 
 ## Serial to Arduino
+import getSensorData as gsd
 import serial
 import time
 import struct
 
-arduino = serial.Serial('/dev/cu.usbmodem1411', 115200, timeout=.1)
-print(arduino.name)
-time.sleep(2)
+ard_connected = False
+
+if(ard_connected):
+    arduino = serial.Serial('/dev/cu.usbmodem1411', 115200, timeout=.1)
+    print(arduino.name)
+    time.sleep(2)
+
+def get_enthalpy():
+    print("get")
+    bench_data = gsd.get_sensor_data('Bench2', 1)
+    return bench_data['Measurements']['Enthalpy']['value']
 
 while True:
 
     ## Get the sensor data
+    ent = get_enthalpy()
+    if(not ard_connected):
+        print(ent)
+        time.sleep(1)
 
     ## recognize if 0 - nothing, 1 - normal, 2 - loyly, 3 - open door
-    ent = 2
 
-    ## Send the data
-    arduino.write(struct.pack('>B', ent))
-    #arduino.write("Hello from Python!")
-    print("sent")
-    data = arduino.readline()
-    print("read")
-    if data:
-        print("data")
-        print(data)  # strip out the new lines for now
-        # (better to do .read() in the long run for this reason
+    if(ard_connected):
+        ## Send the data
+        arduino.write(struct.pack('>B', ent))
+        #arduino.write("Hello from Python!")
+        print("sent")
+        data = arduino.readline()
+        print("read")
+        if data:
+            print("data")
+            print(data)  # strip out the new lines for now
+            # (better to do .read() in the long run for this reason
