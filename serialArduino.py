cd42920a0ca449dbac86c7f4f673c451723d1bd6
karolinadrobotowicz <karolina.drobotowicz@gmail.com>
2018-11-24 21:26:26 +0200

    sending and receiving data works from https and arduino. If you don't use arduino, change the arduino_connected to false

--- serialArduino.py
+++ serialArduino.py
@@ -15,28 +15,35 @@ if(ard_connected):
     time.sleep(2)
 
 def get_enthalpy():
-    print("get")
     bench_data = gsd.get_sensor_data('Bench2', 1)
-    return bench_data['Measurements']['Enthalpy']['value']
+    return bench_data[0]['Measurements']['Enthalpy']['value']
+
+
+def get_enthalpy2():
+    bench_data = gsd.get_sensor_data('Bench2', 1)
+    return bench_data[0]['Measurements']['Temperature']['value']
 
 while True:
 
     ## Get the sensor data
     ent = get_enthalpy()
+    ent2 = get_enthalpy2()
     if(not ard_connected):
         print(ent)
+        print(ent2)
         time.sleep(1)
 
     ## recognize if 0 - nothing, 1 - normal, 2 - loyly, 3 - open door
 
     if(ard_connected):
         ## Send the data
-        arduino.write(struct.pack('>B', ent))
+        arduino.write(str.encode(str(ent)))
         #arduino.write("Hello from Python!")
         print("sent")
         data = arduino.readline()
         print("read")
         if data:
             print("data")
-            print(data)  # strip out the new lines for now
+            print(data.decode())  # strip out the new lines for now
+            time.sleep(2)
             # (better to do .read() in the long run for this reason
