07614a679481ad94188976183771c514a77fbda6
kanatanssi <kanatanssi@gmail.com>
2018-11-25 11:32:30 +0200

    hacked versions of getSensorData and serialArduino for the can, because API is disabled

---
+++ getSensorDataMacHack.py
@@ -0,0 +1,80 @@
+import urllib.request
+import json
+import time
+from datetime import datetime
+import tzlocal
+import ssl
+
+# This restores the same behavior as before.
+context = ssl._create_unverified_context()
+
+
+# Return a list of json object containg the sensor data
+def get_sensor_data(sensorname, number):
+    req = urllib.request.urlopen(urllib.request.Request(
+        "https://apigtw.vaisala.com/hackjunction2018/saunameasurements/latest?SensorID=" +
+        sensorname + "&limit=" + str(number),
+        headers={"Accept": 'application/json'}
+    ), context=context)
+    gcontext = ssl.SSLContext(ssl.PROTOCOL_TLSv1)  # Only for gangstars
+    sensordata = req.read()
+    return json.loads(sensordata)
+
+
+# input: two json measurements, which are the elements of the list returned by get_sensor_data
+# return True if it detects loyly, False if not
+def check_for_loyly(measurement1, measurement2):
+    first_temp = measurement1['Measurements']['Temperature']['value']
+    last_temp = measurement2['Measurements']['Temperature']['value']
+
+    first_local_time = get_localtime_from_measurement(measurement1)
+    second_local_time = get_localtime_from_measurement(measurement2)
+
+    print(first_temp, ' at: ', first_local_time.strftime("%Y-%m-%d %H:%M:%S.%f%z (%Z)"), last_temp, ' at: ',
+          second_local_time.strftime("%Y-%m-%d %H:%M:%S.%f%z (%Z)"))
+
+    if first_temp / last_temp < 0.95:
+        return True
+    else:
+        return False
+
+
+def check_for_opendoor(measurement1, measurement2):
+    first_temp = measurement1['Measurements']['Relative humidity']['value']
+    last_temp = measurement2['Measurements']['Relative humidity']['value']
+
+    first_local_time = get_localtime_from_measurement(measurement1)
+    second_local_time = get_localtime_from_measurement(measurement2)
+
+    print(first_temp, ' at: ', measurement1['Timestamp'], first_local_time.strftime("%Y-%m-%d %H:%M:%S.%f%z (%Z)"),
+    last_temp, ' at: ', measurement1['Timestamp'], second_local_time.strftime("%Y-%m-%d %H:%M:%S.%f%z (%Z)"))
+
+    if last_temp - first_temp > 1.5:
+        return True
+    else:
+        return False
+
+
+def get_localtime_from_measurement(measurement):
+    unix_timestamp = float(str(measurement['Timestamp'])) / 1000
+    local_timezone = tzlocal.get_localzone()
+    local_time = datetime.fromtimestamp(unix_timestamp, local_timezone)
+    return local_time
+
+
+
+# example usage doing the same thing as before
+
+"""while True:
+    sensordata = get_sensor_data('Doorway1', 4)
+    if check_for_opendoor(sensordata[0], sensordata[-1]):
+        print('DOOR OPEN')
+    time.sleep(1)"""
+
+#while True:
+#    sensordata = get_sensor_data('Stove1', 4)
+#    if check_for_opendoor(sensordata[0], sensordata[-1]):
+#        print('DOOR OPEN')
+#    if check_for_loyly(sensordata[0], sensordata[-1]):
+#        print('LOYLY')
+#    time.sleep(1)
---
+++ serialArduinoHack.py
@@ -0,0 +1,113 @@
+# augmenting the experience inside
+# give information outside
+
+## Serial to Arduino
+import getSensorDataMac as gsd
+import consts
+import serial
+import time
+import struct
+from os import system
+
+runningCounter = 0
+fullCounter = 10
+
+ard_connected = True
+
+if(ard_connected):
+    arduino = serial.Serial('/dev/cu.usbmodem1411', 115200, timeout=.1)
+    print(arduino.name)
+    time.sleep(2)
+
+def get_enthalpy():
+    bench_data = gsd.get_sensor_data('Bench2', 1)
+    return bench_data[0]['Measurements']['Enthalpy']['value']
+
+
+def get_enthalpy2():
+    bench_data = gsd.get_sensor_data('Bench2', 1)
+    return bench_data[0]['Measurements']['Temperature']['value']
+
+
+def loyly():
+    sensordata = gsd.get_sensor_data('Stove1', 3)
+    if (len(sensordata) > 1):
+        return gsd.check_for_loyly(sensordata[0], sensordata[-1])
+    else:
+        return False
+
+def doorOpen():
+    sensordata = gsd.get_sensor_data('Doorway1', 4)
+    if(len(sensordata)>1):
+        return gsd.check_for_opendoor(sensordata[0], sensordata[-1])
+    else:
+        return False
+
+
+def check_state():
+
+    if loyly():
+        print("loyly")
+        system("say holy löyly its getting hot!")
+        return consts.states['s_loyly']
+    if doorOpen():
+        print("door open")
+        system("say door opened")
+        return consts.states['s_openDoor']
+
+    ent = get_enthalpy()
+    if ent < consts.maxEnt['s_nothing']:
+        return consts.states['s_nothing']
+
+    if ent < consts.maxEnt['s_heatingup']:
+        print('s_heatingup')
+        system("say s_heatingup")
+        return consts.states['s_heatingup']
+    if ent < consts.maxEnt['s_meh']:
+        print('s_meh')
+        system("say s_meh")
+        return consts.states['s_meh']
+    if ent < consts.maxEnt['s_warm']:
+        print('s_warm')
+        system("say s_warm")
+        return consts.states['s_warm']
+    if ent < consts.maxEnt['s_hot']:
+        print('s_hot')
+        system("say s_hot")
+        return consts.states['s_hot']
+    if ent < consts.maxEnt['s_sizzle']:
+        print('s_sizzle')
+        system("say s_sizzle")
+        return consts.states['s_sizzle']
+    return consts.states['s_hell']
+
+
+
+
+while True:
+
+    ## Check the state
+    #state = check_state()
+    if(runningCounter >= fullCounter):
+        state = '7'
+        runningCounter = 0
+    else:
+        runningCounter = runningCounter + 1
+        state = '0'
+
+    if(not ard_connected):
+        print(state)
+        time.sleep(1)
+
+    ## recognize if 0 - nothing, 1 - normal, 2 - loyly, 3 - open door
+
+    if(ard_connected):
+        ## Send the data
+        arduino.write(str.encode(str(state)))
+        #arduino.write("Hello from Python!")
+        print("Sent state " + str(state) + " to arduino")
+        time.sleep(1)
+        data = arduino.readline()
+        if data:
+            print(data.decode())  # strip out the new lines for now
+            # (better to do .read() in the long run for this reason
